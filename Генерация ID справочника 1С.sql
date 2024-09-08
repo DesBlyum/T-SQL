2341d5b36b63af1d697acc0257192853c40a4dc5
DesBlyum <desblyum@ya.ru>
2024-09-08 15:48:44 +0400

    Переимнование файлов, рефакторинг

---
+++ Генерация ID справочника 1С.sql
@@ -0,0 +1,36 @@
+-- копирование разделов(групп) справочника ...
+-- из-за специфики 1С и настройки БД требуется доп. приседания для переноса данных справочника
+DECLARE @MaxID char(12)
+SELECT @MaxID = dbo.Convert36To10(LEFT(MAX(ID),6)) FROM [dbo].SC18218 WHERE RIGHT(ID,3) = 'ÖÈÁ'
+
+INSERT INTO [SC18218]
+        (ID
+        ,[PARENTID]
+        ,[CODE]
+        ,[DESCR]
+        ,[ISFOLDER]
+        ,[ISMARK]
+        ,[VERSTAMP]
+        ,[SP18215]
+        ,[SP18216]
+        ,[SP18227])
+SELECT
+	dbo.Convert10To36(CAST(ROW_NUMBER() OVER(ORDER BY t.ID) as bigint) + @MaxID) + 'ÖÈÁ' as ID,
+	ParentID,
+	(CODE+200) as CODE,
+	RTRIM(Descr)+'_äèñêàóíòåð',
+	isfolder,
+	ismark,
+	0 as VERSTAMP,
+	SP18215,
+	sp18216,
+	SP18227	
+FROM 
+	SC18218 AS t
+WHERE 
+	ISMARK 		= 0 AND
+	ISFOLDER 	= 1 AND
+	CODE 		< 100
+
+
+
