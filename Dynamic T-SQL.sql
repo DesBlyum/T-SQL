d43ee9e69ebc6463f81cea59f2e5407ac91681a6
DesBlyum <desblyum@ya.ru>
2024-09-15 13:15:07 +0400

    Dynamic T-SQL - рефакторинг и добавлен запрос с использованием QUOTENAME() Добавлен запрос по примеру реализации бинарного поиска по не индексированной колонки

--- Dynamic T-SQL.sql
+++ Dynamic T-SQL.sql
@@ -5,9 +5,9 @@
 ***********************************/
 
 -- —оставление текста запроса со вставками и выполение через EXEC/EXECUTE
-DECLARE @sql varchar(1000)
-DECLARE @columnList varchar(100)
-DECLARE @nameTable varchar(100)
+DECLARE @sql		VARCHAR(1000)
+DECLARE @columnList VARCHAR(100)
+DECLARE @nameTable	VARCHAR(100)
 
 SET @columnList = 'ID'
 SET @nameTable	= 'Test'
@@ -40,5 +40,23 @@ SET @nameTable	= 'Test'
 SET @sqlCommand = 'SELECT @ret = MAX(ID) FROM ' + @nameTable
 
 EXEC sp_executesql @sqlCommand, N'@ret int OUT', @ret = @ret OUT
+
+SELECT @ret
+
+-- ѕередача наименование таблицы в качестве параметра 
+--(используетс€ QUOTENAME() - ¬озвращает строку ёникода с разделител€ми, добавленными, чтобы сделать входную строку допустимым идентификатором разделител€ SQL Server)
+DECLARE @sqlCommand NVARCHAR(1000)
+DECLARE @ParmDef	NVARCHAR(1000)
+DECLARE @nameTable	NVARCHAR(500)
+DECLARE @value		INT
+DECLARE @ret		INT
+
+SET @nameTable = 'Test'
+SET @value	= 5
+
+SET @sqlCommand = N'SELECT @ret = MAX(ID) FROM  '+ QuoteName(@nameTable) + 'WHERE ID = @value'
+SET @ParmDef	= N'@value int, @ret int OUT'
+
+EXEC sp_executesql @sqlCommand, @ParmDef, @value = @value, @ret = @ret OUT
 
 SELECT @ret
